efe8864ed836daeb8fb8ba6427e3e26ed29a74bc
deeh3kr <deepanshu111197@gmail.com>
2020-10-03 15:06:28 +0530

    master

--- src/main/java/com/example/springdemo/SpringdemoApplication.java
+++ src/main/java/com/example/springdemo/SpringdemoApplication.java
@@ -2,7 +2,6 @@ package com.example.springdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 import com.example.springdemo.dao.CourseDao;
 import com.example.springdemo.entities.Course;
